ae6c2a0c4e86c586dc76bbc15c12f14fc8bb6daf
jacky158 <jacky@phpfox.com>
2023-11-19 08:16:41 +0700

    fix

--- packages/acp/src/layouts/Paper.tsx
+++ packages/acp/src/layouts/Paper.tsx
@@ -9,9 +9,9 @@ import ScreenBg from "./ScreenBg";
 
 export default function PaperLayout({ children }: { children: ReactNode }) {
   return (
-    <ScreenCentered>
+    <>
       <ScreenBg image={"/signin-bg-pattern.webp"} />
-      {children}
-    </ScreenCentered>
+      <ScreenCentered children={children} />
+    </>
   );
 }
