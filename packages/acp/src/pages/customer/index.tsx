0e8f4419ac59a2f4671e88290aaa77b5adc9936a
jacky158 <jacky@phpfox.com>
2023-11-21 08:20:18 +0700

    fix

--- packages/acp/src/pages/customer/index.tsx
+++ packages/acp/src/pages/customer/index.tsx
@@ -6,10 +6,16 @@
 import { Layout } from "@ikx/jsx";
 import { Typography } from "@mui/material";
 
-export default function Customer() {
+export default function index(props: unknown) {
+  console.log({ props });
+  console.log(index.loader);
   return (
     <Layout name="layout.master">
-      <Typography variant="h4">Customer</Typography>
+      <Typography variant="h4">Browse </Typography>
     </Layout>
   );
 }
+
+index.loader = async function () {
+  console.log("use loader to fill props");
+};
--- packages/acp/src/pages/customer/view.tsx
+++ packages/acp/src/pages/customer/view.tsx
@@ -1,15 +1,20 @@
 /**
  * @type: route
  * @name: customer
- * @path: /customer/detail
+ * @path: /customer/view/:id
  */
 import { Layout } from "@ikx/jsx";
 import { Typography } from "@mui/material";
 
-export default function Customer() {
+export default function page(props: unknown) {
+  page.loader().then((data) => console.log(props, data));
   return (
     <Layout name="layout.master">
-      <Typography variant="h4">Customer</Typography>
+      <Typography variant="h4">View Customer</Typography>
     </Layout>
   );
 }
+
+page.loader = function () {
+  return Promise.resolve({ name: "Nam Nguyen", Photo: "234" });
+};
